b5e5295d9699a7e0d79d574c663f9fbb370141aa
newSoft5 <92645540+newsoft5@users.noreply.github.com>
2023-07-11 20:33:43 +0300

    asd

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 app.use(express.json())
 
 app.get("/", (req, res) => {
-    res.send("Hello can")
+    res.send("Hello can 3")
 })
 
 app.listen(process.env.PORT || 5000, () => {
