cfc7e59b152c7401811e3603302e749047342d2b
newSoft5 <92645540+newsoft5@users.noreply.github.com>
2023-07-11 21:59:39 +0300

    bootstrap

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-const express = require("express")
+// express
+const express = require("express");
+const app = express();
 
-const app = express()
+// node modules
+const path = require("path");
 
-app.use(express.json())
+// routes
+const userRoutes = require("./routes/user.js")
 
-app.get("/", (req, res) => {
-    console.log("exoo")
-    res.send("Hello can 3")
-})
+// template engine
+app.set("view engine", "ejs");
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log("server")
-})
+// middleware
+app.use(express.urlencoded({ extended: false }))
+
+app.use("/libs", express.static(path.join(__dirname, "node_modules")));
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+app.use(userRoutes);
+
+app.listen(3000, function() {
+    console.log("listening on port 3000");
+});
